8638266e875efc45b2abfe01ac2b50561683d410
Paulo Ferreira <paulo.carvalhoferreira@protonmail.com>
2022-10-21 13:07:26 +0100

    daisy ui config and ladingpage config

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,3 +1,51 @@
-<h1 class = "text-3xl font-bold underline">hello</h1>
+<svelte:head>
+    <title>
+        The Black Wizards
+    </title>
+</svelte:head>
 
-<h1 class = "text-4xl font-bold underline">teste</h1>
+<script>
+
+</script>
+
+<main>
+   
+    <div class="logo">
+        <div class="collapse">
+            <input type="checkbox" /> 
+            <div class="collapse-title text-xl font-medium">
+            The Black Wizards
+            </div>
+            <div class="collapse-content"> 
+                <ul class = "links">
+                    <a href="">Music</a>
+                    <a href="">Tour</a>
+                    <a href="">Merch</a>
+                    <a href="">Media</a>
+                    <a href="">Contact</a>
+                </ul>
+            </div>
+        </div>
+    </div>
+  
+</main>
+
+
+
+<style>
+    .logo{
+        display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 100vh;
+}
+
+    .links {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 1rem;
+    }
+</style>
